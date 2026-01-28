d334833bee1d7145752610e75349664819146451
PIYUSHRAZPUTT <piyushrajpoot1488999@gmail.com>
2026-01-28 18:52:39 +0530

    add functionality

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const formatDoc=(cmd,value=false)=>{
-    if(value){
-        document.execCommand(cmd,false,value);
-    }else{
-        document.execCommand(cmd);
-    }
+const formatDoc = (cmd, value = false) => {
+  if (value) {
+    document.execCommand(cmd, false, value);
+  } else {
+    document.execCommand(cmd);
+  }
 };
 
-const handleAddLink=()=>{
-    const url=prompt("Enter the URL");
-    formatDoc("createLink",url);
-}
+const handleAddLink = () => {
+  const url = prompt("Enter the URL");
+  formatDoc("createLink", url);
+};
+
+const content = document.getElementById("content");
+content.addEventListener("mouseenter", () => {
+  let anchors = content.querySelectorAll("a");
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener("mouseenter", () => {
+      anchor.setAttribute("target", "_blank");
+      content.setAttribute("contentEditable", "false");
+    });
+    anchor.addEventListener("mouseleave", (e) => {
+      content.setAttribute("contentEditable", "true");
+    });
+  });
+});
